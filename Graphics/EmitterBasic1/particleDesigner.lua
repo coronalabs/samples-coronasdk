abc397f053e48c6f04920b4ed5068ef8c333a107
Tom Newman <tom@coronalabs.com>
2016-03-09 19:38:08 -0800

    Changed five graphics samples that use display.newEmitter. Updated particleDesigner.lua file to support subdirectories for texture files

--- Graphics/EmitterBasic1/particleDesigner.lua
+++ Graphics/EmitterBasic1/particleDesigner.lua
@@ -1,8 +1,15 @@
+-- Particle Designer helper file
+--
+-- Modified to support baseDir and textureSubDir
+-- Updated 2016.03.08
+
 local particleDesigner = {}
 
 local json = require "json"
 
-particleDesigner.loadParams = function( filename, baseDir )
+particleDesigner.loadParams = function( filename, baseDir, textureSubDir )
+
+	baseDir = baseDir or system.ResourceDirectory
 
 	-- load file
 	local path = system.pathForFile( filename, baseDir )
@@ -15,14 +22,27 @@ particleDesigner.loadParams = function( filename, baseDir )
 	-- convert json to Lua table
 	local params = json.decode( data )
 
+	if textureSubDir then
+		-- Append the subdirectory to the texture file name if supplied
+		params.textureFileName = textureSubDir .. params.textureFileName
+	end
+
 	return params
 end
 
-particleDesigner.newEmitter = function( filename, baseDir )
+----------------------------------------------------------------------
+-- New Emitter
+--
+-- Returns a new emitter objects
+--   filename = particle data file name (with subdirectory appended)
+--   baseDir = base directory for both particle data file and texture file (optional but must be nil if textureSubDir used)
+--   textureSubDir = subdirectory for texture file (optional)
+--
+particleDesigner.newEmitter = function( filename, baseDir, textureSubDir )
 
-	local emitterParams = particleDesigner.loadParams( filename, baseDir )
+	local emitterParams = particleDesigner.loadParams( filename, baseDir, textureSubDir )
 
-	local emitter = display.newEmitter( emitterParams )
+	local emitter = display.newEmitter( emitterParams, baseDir )	-- for new version of "newEmitter" API
 
 	return emitter
 end
