fb9026428d50703166053774499586e3d7bff2f0
Prathamesh A Degwekar <prat.degwekar@gmail.com>
2017-07-04 13:50:58 +0530

    updated config.lua
    added aspect ratio to modernize the file.
    to add fps and image suffix if required

--- GettingStarted/Clock/config.lua
+++ GettingStarted/Clock/config.lua
@@ -1,11 +1,13 @@
 -- config.lua
 
+local aspectRatio = display.pixelHeight / display.pixelWidth
+
 application =
 {
 	content =
 	{
-		width = 320,
-		height = 480,
+		width = aspectRatio > 1.5 and 320 or math.ceil( 480 / aspectRatio ),
+		height = aspectRatio < 1.5 and 480 or math.ceil( 320 * aspectRatio ), 
 		scale = "letterbox",
 	},
 }
